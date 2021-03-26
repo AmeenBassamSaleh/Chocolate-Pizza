0e80cad8fa81a26f4d3d28e1f977bb5b38290693
AmeenBassamSaleh <ameenbassam222@gmail.com>
2021-03-26 20:07:29 +0300

    lab10a

--- js/app.js
+++ js/app.js
@@ -1,15 +1,38 @@
 'use strict';
-let form =document.getElementById('recipeForm');
-form.addEventListener('change',strictLine);
-function strictLine(event){
-  for (let i=1 ;i<=12;i++){
-    let checkBox1 = document.getElementById('checkbox'+i);
-    let lbl1 = document.getElementById('lbl'+i);
-    if(checkBox1.checked){
-      lbl1.style.textDecoration='line-through';
-      console.log(lbl1);
+// let form =document.getElementById('list');
+// form.addEventListener('change',strictLine);
+// function strictLine(event){
+//   for (let i=1 ;i<=12;i++){
+//     let checkBox1 = document.getElementById('checkbox'+i);
+//     let checkbox = document.getElementById('checkbox'+i);
+//     if(checkBox1.checked){
+//       checkbox.style.textDecoration='line-through';
+//       console.log(checkbox);
+//     }
+//     else
+//     {checkbox.style.textDecoration='unset';
+//     };};
+// }
+//////////////////////////////////////////////////////////////////////
+let my = document.getElementById('list1');
+my.addEventListener('change', strictLine);
+function strictLine(event) {
+
+  console.log(checkbox1);
+
+  for (let i =1 ; i <= 12; i++) {
+    let checkbox1 = document.getElementById('checkbox' + i);
+    let check1 = document.getElementById('check'+i);
+    if (checkbox1.checked) {
+      check1.style.textDecoration = 'line-through';
+      console.log(check1);
     }
-    else{lbl1.style.textDecoration='unset';
-    }}
+    else {
+      check1.style.textDecoration = 'unset';
+    }
+  }
+
 
 }
+// }
+
